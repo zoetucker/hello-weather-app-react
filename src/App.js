6f0161feb674867dfa2babfeb8d2357bfa05c051
zoetucker <78665113+zoetucker@users.noreply.github.com>
2021-05-26 14:51:22 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello Weather</h1>
-      <footer>
-        The project is coded by Zoe Tucker <br />
-        and is open-source on GitHub
-      </footer>
+      <div className="container">
+        <h1>Hello Weather</h1>
+
+        <footer>
+          The project is coded by Zoe Tucker <br />
+          and is open-source on{" "}
+          <a
+            href="https://github.com/zoetucker/hello-weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://sad-northcutt-fdc307.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
