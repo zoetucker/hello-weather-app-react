cea0b575b956dc3cf2ea8c03ed4b399be5e4144a
zoetucker <78665113+zoetucker@users.noreply.github.com>
2021-05-26 14:25:05 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello Weather</div>;
+  return (
+    <div className="App">
+      <h1>Hello Weather</h1>
+      <footer>
+        The project is coded by Zoe Tucker <br />
+        and is open-source on GitHub
+      </footer>
+    </div>
+  );
 }
 
 export default App;
