bf84d6eaad193b7ef417d518e7aeea915eb2b260
zoetucker <78665113+zoetucker@users.noreply.github.com>
2021-06-02 10:25:45 -0400

    updated defaultCity
    from Blacksburg to Bangkok

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Blacksburg" />
+        <Weather defaultCity="Bangkok" />
         <footer>
           The project is coded by Zoe Tucker 👩‍💻 <br />
           and is open-source on{" "}
